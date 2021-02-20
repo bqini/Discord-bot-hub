8c686d3d2a3cb608a7473a78756b0d9a2652411a
Crimson <68981140+ducksquaddd@users.noreply.github.com>
2021-02-19 18:39:09 -0800

    Update managebots.js

--- managebots.js
+++ managebots.js
@@ -1,17 +1,14 @@
-const prompt = require('prompt-sync')();
-const fs = require('fs')
-const sleep = require("sleep")
-const { exec } = require("child_process");
-var shell = require("shelljs")
-var commandsf = "commands"
-const readline = require('readline')
-const blank = '\n'.repeat(process.stdout.rows)
+
+const prompt = require("prompt-sync")();
+var shell = require("shelljs");
 shell.config.verbose = false;
 
-shell.exec('ls -I node_modules -I start.js -I managebots.js -I package.json -I package-lock.json -I app.js -I README.md -I LICENSE')
-const cdfile = prompt('What bot would you like to manage: ');
+shell.exec(
+  "ls -I node_modules -I start.js -I managebots.js -I package.json -I package-lock.json -I app.js -I README.md -I LICENSE"
+);
+const cdfile = prompt("What bot would you like to manage: ");
 
-shell.cd(`${cdfile}`)
+shell.cd(`${cdfile}`);
 
-require('child_process').fork('manage.js');
+require("child_process").fork("manage.js");
 //ls --ignore={"*start.js","*managebots.js","*package.json","*package-lock.json","*node_modules","*LICENSE"}
